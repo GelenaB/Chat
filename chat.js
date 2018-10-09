c9d6ec36393a7fbb4750d6f4823a786d09ec4b83
Gelena Barker <boldireg@tcd.ie>
2018-10-09 08:40:43 +0100

    Renamed functions

--- chat.js
+++ chat.js
@@ -12,85 +12,109 @@
 // End chat or have random messages appear
 
 
-//push enter key (using jquery), to run bot function.
+//when enter is pressed, bubbles will get added
 $(document).keypress(function(e) {
   if (e.which == 13) {
-    createBubble();
-    timer();
+    createUserBubble();
+    createBotBubble();
   }});
 
-function createBubble() {
-
+function createUserBubble() {
   //creates a new div, appends paragraph and time. Applies classes.
-  let newMsg = document.createElement("div");
-  newMsg.classList.add("msgBubble");
+  let newBubble = document.createElement("div");
+  newBubble.classList.add("msgBubble");
 
-  let para = document.createElement("p");
+  let newParagraph = document.createElement("p");
   let input = document.getElementById("input").value;
   let msgContent = document.createTextNode(input);
-  para.appendChild(msgContent);
-  newMsg.appendChild(para);
-
+  newParagraph.appendChild(msgContent);
+  newBubble.appendChild(newParagraph);
 
   let currentTime = document.createElement("span");
   currentTime.classList.add("timeLeft");
   let fullTime = new Date().toTimeString().split(" ")[0].split(":");
   let hoursMins = document.createTextNode(`${fullTime[0]}:${fullTime[1]}`);
   currentTime.appendChild(hoursMins);
-  newMsg.appendChild(currentTime);
+  newBubble.appendChild(currentTime);
 
   let currentDiv = document.getElementById("chatBox");
-  currentDiv.appendChild(newMsg);
+  currentDiv.appendChild(newBubble);
 };
 
-  function timer() {
+  function createBotBubble() {
+
     setTimeout (function() {
 
-    let newMsg = document.createElement("div");
+    let newBubble = document.createElement("div");
     let botName = document.createElement("span");
     botName.classList.add("nameRight");
     let name = document.createTextNode(`Mr Chatty Bot`);
     botName.appendChild(name);
-    newMsg.appendChild(botName);
+    newBubble.appendChild(botName);
 
-    let para = document.createElement("p");
-    newMsg.appendChild(para);
-    newMsg.classList.add("msgBubble");
+    let newParagraph = document.createElement("p");
+    newBubble.appendChild(newParagraph);
+    newBubble.classList.add("msgBubble");
     let msgContent = botMsg();
-    para.appendChild(msgContent);
+    newParagraph.appendChild(msgContent);
 
     let currentTime = document.createElement("span");
     currentTime.classList.add("timeRight");
     let fullTime = new Date().toTimeString().split(" ")[0].split(":");
     let hoursMins = document.createTextNode(`${fullTime[0]}:${fullTime[1]}`);
     currentTime.appendChild(hoursMins);
-    newMsg.appendChild(currentTime);
+    newBubble.appendChild(currentTime);
 
     let currentDiv = document.getElementById("chatBox");
-    currentDiv.appendChild(newMsg);
+    currentDiv.appendChild(newBubble);
     document.getElementById("input").value = "";
     }, 1000);
   }
 
 let botMsgCounter = 0;
+
 function botMsg() {
   botMsgCounter ++;
   let input = document.getElementById("input").value;
+  //let upperCaseInput = capitaliseInput();
   let msgContent = document.createTextNode(input);
   if (botMsgCounter === 1) {
     return document.createTextNode(`Nice to meet you ${input}. Where are you from?`);
   } else if (botMsgCounter === 2) {
-    return document.createTextNode(`Lovely. What's you favourite food?`);
-  } else if (botMsgCounter === 2) {
-    return document.createTextNode(`Oh, ${input} is my favourite too!`);
+    return document.createTextNode(`${input} sounds great! How's your week going?`);
   } else if (botMsgCounter === 3) {
-    return document.createTextNode(`bla`);
+    return document.createTextNode(`Mine's quite busy. Lots of chatting!`);
+  } else if (botMsgCounter === 3) {
+    return document.createTextNode(`And speaking of chatting...`);
+  } else if (botMsgCounter === 3) {
+    return document.createTextNode(`I gotta run. Speak soon`);
+  } else if (botMsgCounter => 4) {
+    return document.createTextNode(`*not available*`);
   }
-}
+};
+
+/*function letterCapitalise() {
+    let input = document.getElementById("input").value;
+    var words = input.split(' '); //we get an array of words
+    //var finalString = ''; //store final result here
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].substr(0,1).toUpperCase() + words[i].substr(1);
+    }
+    return words.join(' '); //join array back into a string
+} */
+
+$('#chatBox').animate({
+ scrollTop: $(".msgBubble:last-child").offset().top
+}, 400);
+
+/*
+$('#chatBox').animate({
+ scrollTop: $(".timeRight:last-child").offset().top
+}, 400); */
 
 //check if this is actually scrolling
-let element = document.getElementById('chatBox');
-element.scrollTop = element.scrollHeight;
+//let element = document.getElementById('chatBox');
+//element.scrollTop = element.scrollHeight;
 
 //var scroll = document.getElementsByClassName('msgBubble');
   // scroll.scrollTop = scroll.scrollHeight;
